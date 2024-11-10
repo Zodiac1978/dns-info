248d2f996b915d0d8ec610eb1d067acf54899de1
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2024-11-10 12:51:51 +0100

    Add text domain

--- inc/dns-checks.php
+++ inc/dns-checks.php
@@ -8,7 +8,7 @@
  */
 function dns_info_register_spf_record_check( $tests ) {
 	$tests['direct']['spf_record'] = array(
-		'label' => __( 'SPF Record Check' ),
+		'label' => __( 'SPF Record Check', 'dns-info' ),
 		'test'  => 'dns_info_spf_record_check',
 	);
 	return $tests;
@@ -23,15 +23,15 @@ add_filter( 'site_status_tests', 'dns_info_register_spf_record_check' );
  */
 function dns_info_spf_record_check() {
 	$result = array(
-		'label'       => __( 'SPF Record is properly configured' ),
+		'label'       => __( 'SPF Record is properly configured', 'dns-info' ),
 		'status'      => 'good',
 		'badge'       => array(
-			'label' => __( 'Security' ),
+			'label' => __( 'Security', 'dns-info' ),
 			'color' => 'blue',
 		),
 		'description' => sprintf(
 			'<p>%s</p>',
-			__( 'Your SPF record is properly configured, helping to prevent email spoofing.' )
+			__( 'Your SPF record is properly configured, helping to prevent email spoofing.', 'dns-info' )
 		),
 		'actions'     => '',
 		'test'        => 'spf_record',
@@ -42,14 +42,14 @@ function dns_info_spf_record_check() {
 
 	if ( empty( $spf_record ) ) {
 		$result['status']      = 'recommended';
-		$result['label']       = __( 'SPF Record is not found' );
+		$result['label']       = __( 'SPF Record is not found', 'dns-info' );
 		$result['description'] = sprintf(
 			'<p>%s</p>',
-			__( 'Your SPF record is not found. It is recommended to create and configure an SPF record to prevent email spoofing.' )
+			__( 'Your SPF record is not found. It is recommended to create and configure an SPF record to prevent email spoofing.', 'dns-info' )
 		);
 		$result['actions']    .= sprintf(
 			'<p>%s</p>',
-			__( 'Please consult your DNS provider or system administrator to create an SPF record for your domain.' )
+			__( 'Please consult your DNS provider or system administrator to create an SPF record for your domain.', 'dns-info' )
 		);
 	}
 
@@ -64,7 +64,7 @@ function dns_info_spf_record_check() {
  */
 function dns_info_register_dmarc_record_check( $tests ) {
 	$tests['direct']['dmarc_record'] = array(
-		'label' => __( 'DMARC Record Check' ),
+		'label' => __( 'DMARC Record Check', 'dns-info' ),
 		'test'  => 'dns_info_dmarc_record_check',
 	);
 	return $tests;
@@ -79,15 +79,15 @@ add_filter( 'site_status_tests', 'dns_info_register_dmarc_record_check' );
  */
 function dns_info_dmarc_record_check() {
 	$result = array(
-		'label'       => __( 'DMARC Record is properly configured' ),
+		'label'       => __( 'DMARC Record is properly configured', 'dns-info' ),
 		'status'      => 'good',
 		'badge'       => array(
-			'label' => __( 'Security' ),
+			'label' => __( 'Security', 'dns-info' ),
 			'color' => 'blue',
 		),
 		'description' => sprintf(
 			'<p>%s</p>',
-			__( 'Your DMARC record is properly configured, helping to prevent email spoofing.' )
+			__( 'Your DMARC record is properly configured, helping to prevent email spoofing.', 'dns-info' )
 		),
 		'actions'     => '',
 		'test'        => 'dmarc_record',
@@ -98,14 +98,14 @@ function dns_info_dmarc_record_check() {
 
 	if ( empty( $dmarc_record ) ) {
 		$result['status']      = 'recommended';
-		$result['label']       = __( 'DMARC Record is not found' );
+		$result['label']       = __( 'DMARC Record is not found', 'dns-info' );
 		$result['description'] = sprintf(
 			'<p>%s</p>',
-			__( 'Your DMARC record is not found. It is recommended to create and configure an DMARC record to prevent email spoofing.' )
+			__( 'Your DMARC record is not found. It is recommended to create and configure an DMARC record to prevent email spoofing.', 'dns-info' )
 		);
 		$result['actions']    .= sprintf(
 			'<p>%s</p>',
-			__( 'Please consult your DNS provider or system administrator to create an DMARC record for your domain.' )
+			__( 'Please consult your DNS provider or system administrator to create an DMARC record for your domain.', 'dns-info' )
 		);
 	}
 
